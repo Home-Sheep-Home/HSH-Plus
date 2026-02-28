dabcf921c23ffb1b92c9c69ead671dc827c68434
Jack <64530581+MrKingPenguin@users.noreply.github.com>
2026-02-28 17:39:14 +0000

    Optimisations

--- Patches/TimerPatches.cs
+++ Patches/TimerPatches.cs
@@ -7,9 +7,7 @@ namespace HSHPlus.Patches
 {
     internal class TimerPatches
     {
-        [HarmonyPatch(typeof(Story_LevelCompletePopup), "UpdateTime")]
-        [HarmonyTranspiler]
-        static IEnumerable<CodeInstruction> UpdateTime_Transpiler(IEnumerable<CodeInstruction> instructions)
+        static IEnumerable<CodeInstruction> AddMilliseconds(IEnumerable<CodeInstruction> instructions)
         {
             var stringIndex = -1;
 
@@ -31,84 +29,34 @@ namespace HSHPlus.Patches
                 codes[stringIndex + 8] = CodeInstruction.Call(typeof(TimeSpan), "get_Milliseconds");
             }
             return codes.AsEnumerable();
+        }
+
+        [HarmonyPatch(typeof(Story_LevelCompletePopup), "UpdateTime")]
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> UpdateTime_Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            return AddMilliseconds(instructions);
         }
 
         [HarmonyPatch(typeof(Story_LevelCompletePopup), "SetupTimings")]
         [HarmonyTranspiler]
         static IEnumerable<CodeInstruction> SetupTimings_Transpiler(IEnumerable<CodeInstruction> instructions)
         {
-            var stringIndex = -1;
-
-            var codes = new List<CodeInstruction>(instructions);
-            for (var i = 0; i < codes.Count; i++)
-            {
-                var strOperand = codes[i].operand as string;
-                if (strOperand == "{0:D2}:{1:D2}")
-                {
-                    stringIndex = i;
-                    break;
-                }
-            }
-            if (stringIndex > -1)
-            {
-                codes[stringIndex].operand = "{0:D2}:{1:D2}.{2:D3}";
-                codes[stringIndex + 7] = CodeInstruction.Call(typeof(string), "Format", [typeof(string), typeof(object), typeof(object), typeof(object)]);
-                codes.InsertRange(stringIndex + 7, codes.GetRange(stringIndex + 1, 3));
-                codes[stringIndex + 8] = CodeInstruction.Call(typeof(TimeSpan), "get_Milliseconds");
-            }
-            return codes.AsEnumerable();
+            return AddMilliseconds(instructions);
         }
 
         [HarmonyPatch(typeof(Story_LevelCompletePopup), "TryStopTweening")]
         [HarmonyTranspiler]
         static IEnumerable<CodeInstruction> TryStopTweening_Transpiler(IEnumerable<CodeInstruction> instructions)
         {
-            var stringIndex = -1;
-
-            var codes = new List<CodeInstruction>(instructions);
-            for (var i = 0; i < codes.Count; i++)
-            {
-                var strOperand = codes[i].operand as string;
-                if (strOperand == "{0:D2}:{1:D2}")
-                {
-                    stringIndex = i;
-                    break;
-                }
-            }
-            if (stringIndex > -1)
-            {
-                codes[stringIndex].operand = "{0:D2}:{1:D2}.{2:D3}";
-                codes[stringIndex + 7] = CodeInstruction.Call(typeof(string), "Format", [typeof(string), typeof(object), typeof(object), typeof(object)]);
-                codes.InsertRange(stringIndex + 7, codes.GetRange(stringIndex + 1, 3));
-                codes[stringIndex + 8] = CodeInstruction.Call(typeof(TimeSpan), "get_Milliseconds");
-            }
-            return codes.AsEnumerable();
+            return AddMilliseconds(instructions);
         }
 
         [HarmonyPatch(typeof(StoryLevelSelectPanel), "SetBestTimeText")]
         [HarmonyTranspiler]
         static IEnumerable<CodeInstruction> SetBestTimeText_Transpiler(IEnumerable<CodeInstruction> instructions)
         {
-            var stringIndex = -1;
-
-            var codes = new List<CodeInstruction>(instructions);
-            for (var i = 0; i < codes.Count; i++)
-            {
-                var strOperand = codes[i].operand as string;
-                if (strOperand == "{0:D2}:{1:D2}")
-                {
-                    stringIndex = i;
-                    break;
-                }
-            }
-            if (stringIndex > -1)
-            {
-                codes[stringIndex].operand = "{0:D2}:{1:D2}.{2:D3}";
-                codes[stringIndex + 7] = CodeInstruction.Call(typeof(string), "Format", [typeof(string), typeof(object), typeof(object), typeof(object)]);
-                codes.InsertRange(stringIndex + 7, codes.GetRange(stringIndex + 1, 3));
-                codes[stringIndex + 8] = CodeInstruction.Call(typeof(TimeSpan), "get_Milliseconds");
-            }
-            return codes.AsEnumerable();
+            return AddMilliseconds(instructions);
         }
     }
 }
