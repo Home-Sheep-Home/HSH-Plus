314cd4df3c98228cf46d73bc792c4a968a0f597e
Jack <64530581+MrKingPenguin@users.noreply.github.com>
2026-03-01 14:44:01 +0000

    Add plugin info to plugin class

--- Plugin.cs
+++ Plugin.cs
@@ -5,16 +5,20 @@ using HSHPlus.Patches;
 
 namespace HSHPlus;
 
-[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInPlugin(GUID, NAME, VERSION)]
 public class Plugin : BaseUnityPlugin
 {
+    private const string GUID = "com.mrkingpenguin.HSHPlus";
+    private const string NAME = "HSH Plus";
+    private const string VERSION = "1.0.0";
+
     internal static Harmony harmony = new("com.mrkingpenguin.HSHPlus");
     internal static new ManualLogSource Logger;
         
     private void Awake()
     {
         Logger = base.Logger;
-        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        Logger.LogInfo($"Plugin {NAME} is loaded!");
         harmony.PatchAll(typeof(TimerPatches));
         harmony.PatchAll(typeof(LevelPatches));
     }
